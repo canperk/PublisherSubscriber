217715b64f059407e64879d0a5fac1bfd61fa383
Can PERK <canperk@outlook.com>
2018-09-04 14:07:39 +0300

    UI design

---
+++ MSAB.PubSub.CanPerk/MSAB.PubSub.CanPerk/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace MSAB.PubSub.CanPerk
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitApp(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Minimize(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void DragWindow(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+    }
+}
