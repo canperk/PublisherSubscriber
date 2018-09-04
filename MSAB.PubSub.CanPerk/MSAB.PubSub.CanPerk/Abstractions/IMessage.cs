4af9e8aa39b50d1b7504ff4ff24d113f828a79da
Can PERK <canperk@outlook.com>
2018-09-05 00:21:40 +0300

    Publisher Subscriber infrastructure

---
+++ MSAB.PubSub.CanPerk/MSAB.PubSub.CanPerk/Abstractions/IMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MSAB.PubSub.CanPerk.Abstractions
+{
+    public interface IMessage
+    {
+        string Title { get; set; }
+        string Content { get; set; }
+        DateTime PublishDate { get; set; }
+    }
+}
---
+++ MSAB.PubSub.CanPerk/MSAB.PubSub.CanPerk/Enums/SubscriberType.cs
@@ -0,0 +1,10 @@
+﻿namespace MSAB.PubSub.CanPerk
+{
+    public enum SubscriberType
+    {
+        Newspaper = 1,
+        Radio = 2,
+        MobileDevice = 3,
+        Internet = 4
+    }
+}
